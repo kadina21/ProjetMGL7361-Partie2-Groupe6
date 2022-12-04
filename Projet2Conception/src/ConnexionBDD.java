59ff004da7097357357a0fc28d9741b15e769cac
kadina21 <93254604+kadina21@users.noreply.github.com>
2022-12-04 17:27:18 -0500

    Import du projet eclipse

---
+++ Projet2Conception/src/ConnexionBDD.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+
+public class ConnexionBDD {
+	private static ConnexionBDD instance = null;
+	public Connection connection = null;
+
+	private ConnexionBDD() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+			System.out.println("DATABASE CONNECTED SUCCESSFULLY");
+   
+		}catch(Exception ex) {
+			System.out.println("DATABASE NOT CONNECTED");
+		}
+	}
+	
+	public static ConnexionBDD getInstance() {
+
+		if (instance == null) {
+			instance = new ConnexionBDD();
+		}
+		
+		return instance;
+	}
+	
+	public static void main(String[] args) {
+		Connection connection = ConnexionBDD.getInstance().connection;
+		PreparedStatement ps;
+		try {
+			ps = connection.prepareStatement("SELECT * FROM utilisateurs;");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				String name = rs.getString("name");
+				String firstName = rs.getString("firstName");								
+				String email = rs.getString("email");
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+				String isAuthor = rs.getString("isAuthor");
+				String field = rs.getString("field");
+				String job = rs.getString("job");
+				System.out.println("name: " + name + ", firstName: " + firstName + ", email: " + email + ", username: " + username + ", password: " + password + ", isAuthor: " + isAuthor + "\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ Projet2Conception/src/test.java
@@ -0,0 +1,12 @@
+
+public class EtatNonSoumis {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public EtatArticle changementEtat() {
+		return new EtatRelecture();
+	}
+
+}
