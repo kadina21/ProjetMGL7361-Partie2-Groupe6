958574fac28f39f396297ea3d5be4c24cef3bdbd
kadina21 <93254604+kadina21@users.noreply.github.com>
2022-12-16 13:15:55 -0500

    Cross origin enabled

--- main/java/com/example/newspaper/controller/ArticleController.java
+++ main/java/com/example/newspaper/controller/ArticleController.java
@@ -2,10 +2,7 @@ package com.example.newspaper.controller;
 
 import com.example.newspaper.outils.GestionAPI;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,30 +10,35 @@ import java.util.List;
 @RequestMapping("/api")
 public class ArticleController {
 
+    @CrossOrigin
     @GetMapping("/articles")
     public List<JSONObject> selectAllArticles(){
         GestionAPI gapi=GestionAPI.getInstance();
         return gapi.encodeJSONArticles();
     }
 
+    @CrossOrigin
     @GetMapping("/articles/{titre}")
     public List<JSONObject> selectArticlesTitre(@PathVariable(value = "titre")String titre){
         GestionAPI gapi=GestionAPI.getInstance();
         return gapi.encodeJSONArticlesByTitle(titre);
     }
 
+    @CrossOrigin
     @GetMapping("/articles_id={id_article}")
     public JSONObject selectIDArticle(@PathVariable(value = "id_article")String id) {
         GestionAPI gapi = GestionAPI.getInstance();
         return gapi.encodeJSONSpecificArticle(id);
     }
 
+    @CrossOrigin
     @GetMapping("/articles_de_{auteur}")
     public List<JSONObject> selectArticlesAuteur(@PathVariable(value = "auteur")String auteur) {
         GestionAPI gapi = GestionAPI.getInstance();
         return gapi.encodeJSONArticlesByAuthor(auteur);
     }
 
+    @CrossOrigin
     @GetMapping("/articles/cat={categorie}")
     public List<JSONObject> selectArticlesCategorie(@PathVariable(value = "categorie")String categorie) {
         GestionAPI gapi = GestionAPI.getInstance();
--- main/java/com/example/newspaper/controller/ScientifiqueController.java
+++ main/java/com/example/newspaper/controller/ScientifiqueController.java
@@ -2,10 +2,7 @@ package com.example.newspaper.controller;
 
 import com.example.newspaper.outils.GestionAPI;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,18 +10,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class ScientifiqueController {
 
+    @CrossOrigin
     @GetMapping("/users")
     public List<JSONObject> selectAllUsers(){
         GestionAPI gapi=GestionAPI.getInstance();
         return gapi.encodeJSONUsers();
     }
 
+    @CrossOrigin
     @GetMapping("/users/{type_user}")
     public List<JSONObject> selectTypeUser(@PathVariable(value = "type_user")String type){
         GestionAPI gapi=GestionAPI.getInstance();
         return gapi.encodeJSONTypeUser(type);
     }
 
+    @CrossOrigin
     @GetMapping("/user/{id_user}")
     public JSONObject selectIDUser(@PathVariable(value = "id_user")String id){
         GestionAPI gapi=GestionAPI.getInstance();
